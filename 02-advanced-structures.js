4a2784d27cb3c262481acf26fb08784447310765
davidfnino0624 <davidnino0624@gmail.com>
2025-07-14 22:08:27 -0500

    continuacion de estructuras avanzadas

--- 02-advanced-structures.js
+++ 02-advanced-structures.js
@@ -53,4 +53,55 @@ console.log(flatArray)
         
 let phrases = [`hola mundo`, `adios mundo`]
 
-phrases.flatMap(phrase => phrase.split(' '))
+let words =  phrases.flatMap(phrase => phrase.split(' '))
+
+console.log(words)
+
+        // -Ordenacion
+
+            // sort
+
+let unsorted = ["b", "a", "d", "c"]
+let sorted = unsorted.sort()
+console.log(sorted)
+
+unsorted = [3, 4, 1, 6, 10]
+sorted = unsorted.sort((a, b) => a - b)
+
+console.log(sorted)
+
+            // reverse
+
+sorted.reverse()
+console.log(sorted)
+
+            // Busqueda
+
+console.log(sorted.includes(4))
+console.log(sorted.includes(5))
+
+            // find
+    
+let firstEvenNumber = sorted.find(elementoz => elementoz % 2 === 0)
+
+console.log(firstEvenNumber)
+
+            // findIndex
+
+let firstEvenIndex = sorted.findIndex(elementoz => elementoz % 2 === 0)
+
+console.log(firstEvenIndex)
+
+
+    // SetsAvanzados
+
+        // - Operaciones
+
+            // eliminacion de duplicados
+
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 2, 3, 4, 7]
+const numbersSet = [...new Set(numbersArray)]
+
+console.log(numbersSet)
+
+            // union
